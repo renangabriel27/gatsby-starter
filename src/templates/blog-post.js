dbe620cb65f4e8b31d445c042472c729678a0793
Renan Gabriel <renanthompsom@gmail.com>
2020-08-23 12:50:54 -0300

    Add post component with your styled.js

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,8 +1,10 @@
-import React from 'react'
-import { graphql } from 'gatsby'
+import React from "react"
+import { graphql } from "gatsby"
 
 import Layout from "../components/Layout"
 import SEO from "../components/seo"
+
+import * as S from "../components/Post/styled"
 
 const BlogPost = ({ data }) => {
   const post = data.markdownRemark
@@ -10,19 +12,34 @@ const BlogPost = ({ data }) => {
   return (
     <Layout>
       <SEO title={post.frontmatter.title} />
-      <h1>{post.frontmatter.title}</h1>
-      <div dangerouslySetInnerHTML={{ __html: post.html}}></div>
+
+      <S.PostHeader>
+        <S.PostDate>
+          {post.frontmatter.date} • {post.timeToRead} min de leitura
+        </S.PostDate>
+
+        <S.PostTitle>{post.frontmatter.title}</S.PostTitle>
+
+        <S.PostDescription>{post.frontmatter.description}</S.PostDescription>
+      </S.PostHeader>
+
+      <S.MainContent>
+        <div dangerouslySetInnerHTML={{ __html: post.html }}></div>
+      </S.MainContent>
     </Layout>
   )
 }
 
 export const query = graphql`
-  query Post($slug: String) {
-    markdownRemark(fields: {slug: {eq: $slug}}) {
+  query Post($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
       frontmatter {
         title
+        description
+        date(locale: "pt-br", formatString: "DD [de] MMMM [de] YYYY")
       }
       html
+      timeToRead
     }
   }
 `
